0f68a4242d2ad3cc71e31c686f0adeb0e689d4ad
Sebastian Kramer <sebastian.kramer@wp.pl>
2015-01-27 11:57:02 +0100

    added tracing info

--- git-pull-all.sh
+++ git-pull-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while read F; do
     cd $F
+    echo -e "\n"$F
     git pull
 done </home/skramer/scripts/git-repos
--- git-status-all.sh
+++ git-status-all.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while read F; do
     cd $F
+    echo -e "\n"$F
     git status
 done </home/skramer/scripts/git-repos
