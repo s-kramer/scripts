2e517978c77ee99a2398f4b8be9b490fa70337b5
Sebastian Kramer <sebastian.kramer@wp.pl>
2015-02-04 20:31:29 +0100

    added git-executable-dirs file and modified gh script accordingly

--- git-add-commit-push.sh
+++ git-add-commit-push.sh
@@ -5,7 +5,25 @@ if [[ "$#" -ne 1 ]]; then
     exit 1;
 fi
 
-if [[ "$PWD" != "/home/skramer/scripts" ]]; then
+in_array() { #{{{
+    local haystack=${1}[@]
+    local needle=${2}
+    for i in ${!haystack}; do
+        if [[ "${i}" == "${needle}" ]]; then
+            return 0
+        fi
+    done
+    return 1
+} 
+#}}}
+
+declare -a executable_dirs
+readarray -t executable_dirs < /home/skramer/scripts/git-executable-dirs
+
+in_array executable_dirs "$PWD"
+
+# if not found
+if [[ $? -eq 1 ]]; then
     reload-git-ignore.sh
 fi
 
